2dafcb6b95dbee5ff5e2519182b39c44c1021897
leelike123 <leelike123@email.daelim.ac.kr>
2024-10-02 14:42:16 +0900

    Question

--- src/Question8.java
+++ src/Question8.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 
 public class Question8 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("몸무게를 입력해주세요 : ");
+        double weight = sc.nextInt();
+        System.out.print("키를 입력해주세요 : ");
+        double height = sc.nextInt();
 
-        System.out.print("키를 입력하세요 (m): ");
-        double height = Double.parseDouble(sc.next());
-        System.out.print("몸무게를 입력하세요 (kg): ");
-        int weight = sc.nextInt();
-        System.out.print("BMI 수치 : " + weight / (height * height));
-        float health = sc.nextInt();
-        System.out.print(health);
+        double bmi = weight / (height * height);
 
-        if (health>=30) {
-            System.out.print("비만입니다.");
-        } else if (health<18.5) {
-            System.out.print("저체중입니다.");
-        } else if (health>=18.5 && health<=24.9) {
-            System.out.print("정상입니다.");
-        } else if (health>=24.9 && health<=29.9) {
-            System.out.print("과체중입니다.");
+        if(bmi < 18.5){
+            System.out.println("저체중입니다.");
+        }else if(bmi < 24.9){
+            System.out.println("정상입니다.");
+        }else if(bmi < 29.9) {
+            System.out.println("과체중입니다.");
+        }else{
+            System.out.println("비만입니다.");
         }
     }
 }
-
